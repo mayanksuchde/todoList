8070ea62426a58d91d8ca8a465d34a4dc0563bca
LAPTOP-M6R4B458\mitsa\mayank <mayank.suchde.4500@gmail.com>
2017-12-30 19:27:00 -0500

    add create todo function

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,12 +1,21 @@
 //Check Off Specific Todos By Clicking
-$("li").click(function(){
+$("ul").on("click","li",function(){
 	$(this).toggleClass("completed");
 });
 
-$("span").click(function(event){
+$("ul").on("click","span",function(event){
 	$(this).parent().fadeOut(500,function(){
 		$(this).remove();
 	});
 	event.stopPropogation(); 
+});
+
+$("input[type='text']").keypress(function(event) {
+	/* Act on the event */
+	if (event.which===13) {
+		var todoText= $(this).val();
+		$(this).val("");
+		$("ul").append('<li><span>X</span> ' + todoText + ' </li> ');
+	}
 });
 
