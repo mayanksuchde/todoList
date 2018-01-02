0d7e054f355cb2628d5b7a582affc7d7c994c905
LAPTOP-M6R4B458\mitsa\mayank <mayank.suchde.4500@gmail.com>
2018-01-02 18:14:01 -0500

    added styling, icons for trash and adding a todo

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -15,7 +15,13 @@ $("input[type='text']").keypress(function(event) {
 	if (event.which===13) {
 		var todoText= $(this).val();
 		$(this).val("");
-		$("ul").append('<li><span>X</span> ' + todoText + ' </li> ');
+		$("ul").append('<li><span><i class="fa fa-trash"></i></span> ' + todoText + ' </li> ');
 	}
+});
+
+
+$(".fa-plus").click(function(event) {
+	/* Act on the event */
+	$("input[type='text']").fadeToggle();
 });
 
